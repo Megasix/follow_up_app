ca252eb8e5ad35b5f75bc99ee82fca7a5ee913d5
Megasix <75232015+Megasix@users.noreply.github.com>
2021-02-28 22:57:35 -0500

    Update constants.dart
    theme colors updated

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -8,14 +8,16 @@ final lightThemeConstant = ThemeData(
   scaffoldBackgroundColor: Color(0xFFF5F5F5),
   buttonColor: Color(0xFFFBAF00),
   primaryColor: Color(0xFFFBAF00),
+  secondaryHeaderColor: Color(0xFFFBAF00),
   brightness: Brightness.light,
-  accentColor: Color(0xFF1A1F23),
+  accentColor: Colors.black,
 );
 
 final darkThemeConstant = ThemeData(
   scaffoldBackgroundColor: Color(0xFF1A1F23),
   buttonColor: Color(0xFFFFAA33),
   primaryColor: Color(0xFFFFAA33),
+  secondaryHeaderColor: Colors.grey[700],
   brightness: Brightness.dark,
-  accentColor: Colors.grey,
+  accentColor: Colors.white,
 );
