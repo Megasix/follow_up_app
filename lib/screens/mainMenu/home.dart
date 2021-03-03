eb443f2da6d98bd5ec56acf4b962aadee52977bd
Megasix <75232015+Megasix@users.noreply.github.com>
2021-03-02 20:00:22 -0500

    Create home.dart
    Création du fichier et ajout de la base visuelle de la page

---
+++ lib/screens/mainMenu/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: 75.0),
+      child: Container(
+        padding: EdgeInsets.only(bottom: 40.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(50.0),
+            topRight: Radius.circular(50.0),
+          ),
+          color: Theme.of(context).scaffoldBackgroundColor,
+        ),
+      ),
+    );
+  }
+}
