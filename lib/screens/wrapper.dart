1bf68aaca285c7466075f5341281e14ed09933f4
Megasix <75232015+Megasix@users.noreply.github.com>
2021-03-02 19:57:31 -0500

    Update wrapper.dart
    1. Modification de l'import suite à la modification du nom du fichier home.dart

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:follow_up_app/models/user.dart';
 import 'package:follow_up_app/screens/authenticate/authenticate.dart';
-import 'package:follow_up_app/screens/home/home.dart';
+import 'package:follow_up_app/screens/mainMenu/mainMenu.dart';
 import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
@@ -15,6 +15,6 @@ class Wrapper extends StatelessWidget {
     if (user == null)
       return Authenticate();
     else
-      return Home();
+      return MainMenu();
   }
 }
