fca25053629d7601f0387f081b12efe495f583c5
Megasix <75232015+Megasix@users.noreply.github.com>
2021-03-05 21:08:55 -0500

    Update messaging.dart
    ajout du widget Text a des fins de reconnaissance

--- lib/screens/mainMenu/messaging.dart
+++ lib/screens/mainMenu/messaging.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class Messaging extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(child: Text('Message'),);
   }
 }
