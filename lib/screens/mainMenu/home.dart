654d6ab7a7a8e0e10282e51d775c79cbdd44eba4
Olivier <olivier_Dery-prevost@hotmail.com>
2021-03-12 11:20:07 -0500

    affiche position actualisé

--- lib/screens/mainMenu/home.dart
+++ lib/screens/mainMenu/home.dart
@@ -3,10 +3,24 @@ import 'package:follow_up_app/services/auth.dart';
 import 'package:follow_up_app/services/database.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
+import 'package:intl/intl.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Location _currentPosition;
+  String _address, _dateTime;
   bool status;
   final AuthService _authService = AuthService();
+  @override
+  void initState() {
+    super.initState();
+    if (status != false) _determinePosition();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -32,7 +46,9 @@ class Home extends StatelessWidget {
                       status = true;
                       print("Start");
                       _determinePosition().then((location) {
-                        print(location);
+                        _geocodePosition(location).then((address) {
+                          print(address);
+                        });
                       });
                     },
                     child: Text("Start"),
@@ -41,7 +57,6 @@ class Home extends StatelessWidget {
                     onPressed: () {
                       status = false;
                       print("Stop");
-                      _authService.signOut();
                     },
                     child: Text("Stop"),
                   ),
@@ -51,53 +66,77 @@ class Home extends StatelessWidget {
             Expanded(
               child: Column(
                 children: <Widget>[
-                  Text('Location'),
+                  SizedBox(
+                    height: 3,
+                  ),
+                  if (_dateTime != null)
+                    Text(
+                      "Date/Time: $_dateTime",
+                    ),
+                  SizedBox(
+                    height: 3,
+                  ),
+                  if (_address != null)
+                    Text(
+                      "Address: $_address",
+                    ),
                 ],
               ),
+            ),
+            RaisedButton(
+              onPressed: _authService.signOut,
+              child: Text("signout"),
             ),
           ],
         ),
       ),
     );
   }
-}
 
-Future<String> _determinePosition() async {
-  bool serviceEnabled;
-  LocationPermission permission;
+  _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
 
-  // Test if location services are enabled.
-  serviceEnabled = await Geolocator.isLocationServiceEnabled();
-  if (!serviceEnabled) {
-    print("pas service");
-  }
-
-  permission = await Geolocator.checkPermission();
-  if (permission == LocationPermission.denied) {
-    print("demande");
-    permission = await Geolocator.requestPermission();
-    if (permission == LocationPermission.deniedForever) {
-      print("tjr non");
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      print("pas service");
     }
 
+    permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
-      print("refuser");
-    }
-  }
-  Position coordinate = await Geolocator.getCurrentPosition();
-  List<Placemark> placemarks =
-      await placemarkFromCoordinates(coordinate.latitude, coordinate.longitude);
-  Placemark place = placemarks[0];
-  String _currentAddress =
-      "${place.street},${place.locality}, ${place.postalCode}, ${place.country}";
-  return _currentAddress;
-}
+      print("demande");
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.deniedForever) {
+        print("tjr non");
+      }
 
-Future<String> _geocodePosition(Position coordinate) async {
-  List<Placemark> placemarks =
-      await placemarkFromCoordinates(coordinate.latitude, coordinate.longitude);
-  Placemark place = placemarks[0];
-  String _currentAddress =
-      "${place.street},${place.locality}, ${place.postalCode}, ${place.country}";
-  return _currentAddress;
+      if (permission == LocationPermission.denied) {
+        print("refuser");
+      }
+    }
+    Position coordinate = await Geolocator.getCurrentPosition();
+
+    setState(() {
+      DateTime now = DateTime.now();
+      _dateTime = DateFormat('EEE d MMM kk:mm:ss ').format(now);
+      _geocodePosition(coordinate).then((place) {
+        setState(() {
+          _address = place;
+        });
+        print(_address);
+      });
+    });
+
+    return coordinate;
+  }
+
+  _geocodePosition(Position coordinate) async {
+    List<Placemark> placemarks = await placemarkFromCoordinates(
+        coordinate.latitude, coordinate.longitude);
+    Placemark place = placemarks[0];
+    String _currentAddress =
+        "${place.street},${place.locality}, ${place.postalCode}, ${place.country}";
+    return _currentAddress;
+  }
 }
