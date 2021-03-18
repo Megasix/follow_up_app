2d74bdcb2c8b952ba870e0b7bbe189460a2c51f4
Olivier <olivier_Dery-prevost@hotmail.com>
2021-03-18 15:33:09 -0400

    test map

--- lib/screens/mainMenu/home.dart
+++ lib/screens/mainMenu/home.dart
@@ -5,7 +5,9 @@ import 'package:follow_up_app/services/auth.dart';
 import 'package:follow_up_app/services/database.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:intl/intl.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -13,16 +15,33 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  Location _currentPosition;
   String _address, _dateTime;
-  bool status;
-  StreamSubscription _locationSubscription;
+  bool status = true;
   final AuthService _authService = AuthService();
-
+  Map<PolylineId, Polyline> polylines = {};
+  List<LatLng> polylineCoordinates = [];
+  PolylinePoints polylinePoints = PolylinePoints();
+  Position positioninit;
+  Completer<GoogleMapController> _controller = Completer();
+  static final CameraPosition _camPos = CameraPosition(
+    target: LatLng(45.5167, -73.65),
+    zoom: 14.4746,
+  );
   @override
   void initState() {
     super.initState();
-    if (status != false) _determinePosition();
+
+    StreamSubscription<Position> positionStream =
+        Geolocator.getPositionStream().listen((Position position) {
+      setState(() {
+        _geocodePosition(position).then((value) {
+          _address = value;
+          print(position.speed);
+        });
+        DateTime now = DateTime.now();
+        _dateTime = DateFormat('EEE d MMM kk:mm:ss ').format(now);
+      });
+    });
   }
 
   @override
@@ -49,6 +68,7 @@ class _HomeState extends State<Home> {
                     onPressed: () {
                       status = true;
                       print("Start");
+                      positioninit = _determinePosition();
                     },
                     child: Text("Start"),
                   ),
@@ -56,6 +76,9 @@ class _HomeState extends State<Home> {
                     onPressed: () {
                       status = false;
                       print("Stop");
+                      _determinePosition().then((value) {
+                        _getPolyline(value);
+                      });
                     },
                     child: Text("Stop"),
                   ),
@@ -114,17 +137,8 @@ class _HomeState extends State<Home> {
         print("refuser");
       }
     }
-    // ignore: cancel_subscriptions
-    StreamSubscription<Position> positionStream =
-        Geolocator.getPositionStream().listen((Position position) {
-      setState(() {
-        _geocodePosition(position).then((value) {
-          _address = value;
-        });
-        DateTime now = DateTime.now();
-        _dateTime = DateFormat('EEE d MMM kk:mm:ss ').format(now);
-      });
-    });
+    Position position = await Geolocator.getCurrentPosition();
+    return position;
   }
 
   _geocodePosition(Position coordinate) async {
@@ -134,5 +148,27 @@ class _HomeState extends State<Home> {
     String _currentAddress =
         "${place.street},${place.locality}, ${place.postalCode}, ${place.country}";
     return _currentAddress;
+  }
+
+  _addPolyLine() {
+    PolylineId id = PolylineId("poly");
+    Polyline polyline = Polyline(
+        polylineId: id, color: Colors.red, points: polylineCoordinates);
+    polylines[id] = polyline;
+    setState(() {});
+  }
+
+  _getPolyline(Position position) async {
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+        "AIzaSyCE2L8QxWYSXlydQlVCsLpkqt9e8B8N080",
+        PointLatLng(positioninit.latitude, positioninit.longitude),
+        PointLatLng(position.latitude, position.longitude),
+        travelMode: TravelMode.walking);
+    if (result.points.isNotEmpty) {
+      result.points.forEach((PointLatLng point) {
+        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+      });
+    }
+    _addPolyLine();
   }
 }
