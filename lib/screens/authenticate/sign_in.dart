67ec2476fb5421d5806d46e83cce25083660d104
Megasix <75232015+Megasix@users.noreply.github.com>
2021-03-26 15:45:22 -0400

    Update sign_in.dart
    Réajustement des positions des widgets avec un système de ratio

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
@@ -46,141 +47,148 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     //default gap between two elements
-    const sameTypePadding = 10.0;
-    const generalPadding = 20.0;
+    const _referenceHeight = 820.5714285714286;
+    const _referenceWidth = 411.42857142857144;
+    final double contextHeight = MediaQuery.of(context).size.height;
     final double contextWidth = MediaQuery.of(context).size.width;
+    final double contextAspectRatio = MediaQuery.of(context).devicePixelRatio;
+    var sameTypeVerticalPadding = 10.0 * contextHeight / _referenceHeight;
+    var generalVerticalPadding = 30.0 * contextHeight / _referenceHeight;
+    var heightRatio = contextHeight / _referenceHeight;
+    var widthRatio = contextWidth / _referenceWidth;
+    print(heightRatio);
 
     bool lightThemeEnabled = getTheme();
 
     return loading
         ? Loading()
         : Scaffold(
-            resizeToAvoidBottomInset: false,
-            body: AnimatedContainer(
-              duration: Duration(milliseconds: 400),
-              curve: Curves.easeOut,
-              alignment: childAlignement,
-              color: Theme.of(context).secondaryHeaderColor,
-              width: double.infinity,
-              height: double.infinity,
-              child: FormBuilder(
-                key: _formKey,
-                autovalidateMode: AutovalidateMode.always,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    SizedBox(height: 75),
-                    Image(
-                      image: AssetImage(
-                        "assets/images/${lightThemeEnabled ? "Dark_" : ""}Follow_Up_logo-01.png",
-                      ),
-                      height: 200.0,
-                      width: 200.0,
-                    ),
-                    SizedBox(height: 50),
-                    Expanded(
-                      child: Container(
-                        padding: EdgeInsets.all(40.0),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(50.0),
-                            topRight: Radius.circular(50.0),
+          resizeToAvoidBottomInset: false,
+          body: AnimatedContainer(
+            duration: Duration(milliseconds: 400),
+            curve: Curves.easeOut,
+            alignment: childAlignement,
+            color: Theme.of(context).secondaryHeaderColor,
+            width: double.infinity,
+            height: double.infinity,
+            child: FormBuilder(
+              key: _formKey,
+              autovalidateMode: AutovalidateMode.always,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  SizedBox(
+                    height: 350.0*heightRatio,
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 75.0*heightRatio, bottom: 50.0*heightRatio, left: 50.0*widthRatio, right: 50.0*widthRatio),
+                      child: AspectRatio(
+                        aspectRatio: contextAspectRatio,
+                        child: Image(
+                          image: AssetImage(
+                            "assets/images/${lightThemeEnabled ? "Dark_" : ""}Follow_Up_logo-01.png",
                           ),
-                          color: Theme.of(context).backgroundColor,
-                        ),
-                        child: Column(
-                          children: <Widget>[
-                            FormBuilderTextField(
-                              name: 'Account Email',
-                              decoration: textInputDecoration.copyWith(hintText: 'Account Email'),
-                              keyboardType: TextInputType.text,
-                              validator: FormBuilderValidators.compose([
-                                FormBuilderValidators.required(context),
-                                FormBuilderValidators.email(context),
-                              ]),
-                              onChanged: (val) {
-                                setState(() => email = val);
-                              },
-                            ),
-                            SizedBox(height: sameTypePadding),
-                            // password field
-                            FormBuilderTextField(
-                              name: 'Password',
-                              decoration: textInputDecoration.copyWith(hintText: 'Password'),
-                              obscureText: true,
-                              keyboardType: TextInputType.text,
-                              validator: FormBuilderValidators.compose([
-                                FormBuilderValidators.required(context),
-                                FormBuilderValidators.maxLength(context, 20),
-                                FormBuilderValidators.minLength(context, 5),
-                              ]),
-                              onChanged: (val) {
-                                setState(() => password = val);
-                              },
-                            ),
-                            SizedBox(height: generalPadding),
-
-                            SizedBox(
-                              width: contextWidth,
-                              height: 40.0,
-                              child: RaisedButton(
-                                  child: Text('Sign In', style: TextStyle(color: Colors.white)),
-                                  onPressed: () async {
-                                    setState(() => loading = true);
-                                    dynamic result = await _authService.signInWithEmailAndPassword(email, password);
-                                    if (result == null)
-                                      setState(() {
-                                        error = 'There was an error using these credential please retry';
-                                        loading = false;
-                                      });
-                                  }),
-                            ),
-
-                            SizedBox(height: generalPadding),
-
-                            Text('OR SIGN IN WITH'),
-
-                            SizedBox(height: generalPadding),
-
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                GoogleSignInButton(onPressed: () async {
-                                  await _authService.signInWithGoogle();
-                                }),
-                                SizedBox(width: sameTypePadding),
-                                FacebookSignInButton(onPressed: () async {
-                                  await _authService.signInWithFacebook();
-                                }),
-                                SizedBox(width: sameTypePadding),
-                                TwitterSignInButton(onPressed: () async {}),
-                                SizedBox(width: sameTypePadding),
-                                AppleSignInButton(onPressed: () async {}, darkMode: !lightThemeEnabled),
-                              ],
-                            ),
-
-                            SizedBox(height: generalPadding),
-
-                            FlatButton(
-                              child: Text('CREATE A FREE ACCOUNT'),
-                              onPressed: () {
-                                widget.toggleView();
-                              },
-                            ),
-
-                            SizedBox(height: 8.0),
-                            Text(
-                              error,
-                              style: TextStyle(color: Colors.red, fontSize: 14.0),
-                            ),
-                          ],
                         ),
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.only(top: 40.0*heightRatio, right: 40.0*widthRatio, left: 40.0*widthRatio),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(50.0),
+                          topRight: Radius.circular(50.0),
+                        ),
+                        color: Theme.of(context).backgroundColor,
+                      ),
+                      child: Column(
+
+                        children: <Widget>[
+                          FormBuilderTextField(
+                            name: 'Account Email',
+                            decoration: textInputDecoration.copyWith(hintText: 'Account Email'),
+                            keyboardType: TextInputType.text,
+                            validator: FormBuilderValidators.compose([
+                              FormBuilderValidators.required(context),
+                              FormBuilderValidators.email(context),
+                            ]),
+                            onChanged: (val) {
+                              setState(() => email = val);
+                            },
+                          ),
+                          SizedBox(height: sameTypeVerticalPadding),
+                          // password field
+                          FormBuilderTextField(
+                            name: 'Password',
+                            decoration: textInputDecoration.copyWith(hintText: 'Password'),
+                            obscureText: true,
+                            keyboardType: TextInputType.text,
+                            validator: FormBuilderValidators.compose([
+                              FormBuilderValidators.required(context),
+                              FormBuilderValidators.maxLength(context, 20),
+                              FormBuilderValidators.minLength(context, 5),
+                            ]),
+                            onChanged: (val) {
+                              setState(() => password = val);
+                            },
+                          ),
+                          SizedBox(height: generalVerticalPadding),
+
+                          SizedBox(
+                            width: contextWidth,
+                            height: 40.0,
+                            child: RaisedButton(
+                                child: Text('Sign In', style: TextStyle(color: Colors.white)),
+                                onPressed: () async {
+                                  setState(() => loading = true);
+                                  dynamic result = await _authService.signInWithEmailAndPassword(email, password);
+                                  if (result == null)
+                                    setState(() {
+                                      error = 'There was an error using these credential please retry';
+                                      loading = false;
+                                    });
+                                }),
+                          ),
+
+                          SizedBox(height: generalVerticalPadding),
+
+                          Text('OR SIGN IN WITH'),
+
+                          SizedBox(height: generalVerticalPadding),
+
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              GoogleSignInButton(onPressed: () async {
+                                await _authService.signInWithGoogle();
+                              }),
+                              SizedBox(width: sameTypeVerticalPadding),
+                              FacebookSignInButton(onPressed: () async {
+                                await _authService.signInWithFacebook();
+                              }),
+                              SizedBox(width: sameTypeVerticalPadding),
+                              TwitterSignInButton(onPressed: () async {}),
+                              SizedBox(width: sameTypeVerticalPadding),
+                              AppleSignInButton(onPressed: () async {}, darkMode: !lightThemeEnabled),
+                            ],
+                          ),
+
+                          SizedBox(height: generalVerticalPadding),
+
+                          FlatButton(
+                            child: Text('CREATE A FREE ACCOUNT'),
+                            onPressed: () {
+                              widget.toggleView();
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          );
+          ),
+        );
   }
 }
