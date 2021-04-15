fa46f198118ea72e12043abf300543ca181e64a5
Megasix <75232015+Megasix@users.noreply.github.com>
2021-04-15 13:18:28 -0400

    Update user.dart
    Mise à jour de UserData et création de UsersRecipient

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,3 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+
 class CustomUser {
   final String uid;
 
@@ -5,16 +8,17 @@ class CustomUser {
 }
 
 class UserData {
-  final String uid;
-  final String name;
-  final String hobby;
-  final int age;
+  String firstName;
+  String lastName;
+  String country;
+  String email;
+  String phoneNumber;
+  Timestamp birthDate;
+  String profilePictureAdress;
 
-  UserData({this.uid, this.name, this.hobby, this.age});
+  UserData(this.firstName, this.lastName, this.country, this.email, this.phoneNumber, this.birthDate, this.profilePictureAdress);
 }
 
-class UserDisplaySetting{
-  final bool theme;
-
-  UserDisplaySetting(this.theme);
+class UsersRecipient extends UserData {
+  UsersRecipient(String firstName, String lastName, String country, String email, String phoneNumber, Timestamp birthDate, String profilePictureAdress) : super(firstName, lastName, country, email, phoneNumber, birthDate, profilePictureAdress);
 }
