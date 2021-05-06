984259190ff985a91e84f28c2a11797653d94d03
Megasix <75232015+Megasix@users.noreply.github.com>
2021-05-06 19:33:50 -0400

    Creation de trajet

---
+++ lib/screens/mainMenu/rides/tracker.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Ride extends StatefulWidget {
+  @override
+  _RideState createState() => _RideState();
+}
+
+class _RideState extends State<Ride> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
