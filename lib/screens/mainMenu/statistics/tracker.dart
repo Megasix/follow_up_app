83de62fab797e1843a33a29a38934a72e6c7f1c2
Megasix <75232015+Megasix@users.noreply.github.com>
2021-05-15 18:33:01 -0400

    change tracker path

---
+++ lib/screens/mainMenu/statistics/tracker.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class RideMap extends StatefulWidget {
+  const RideMap({Key key}) : super(key: key);
+
+  @override
+  _RideMapState createState() => _RideMapState();
+}
+
+class _RideMapState extends State<RideMap> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
