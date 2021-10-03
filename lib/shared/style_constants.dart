478fc440cf6f1c98459520852e60af82ff7867ce
Megasix <75232015+Megasix@users.noreply.github.com>
2021-10-03 17:48:48 -0400

    Update style_constants.dart
    I started to set up the new system of constants for the Themes. It's inspired by this [github](https://github.com/ApplETS/Notre-Dame/blob/master/lib/ui/utils/app_theme.dart)

--- lib/shared/style_constants.dart
+++ lib/shared/style_constants.dart
@@ -4,6 +4,47 @@ const textInputDecoration = InputDecoration(
   focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.blueGrey, width: 2.0)),
 );
 
+class AppTheme {
+  AppTheme._();
+
+  //Backgrounds
+  static const Color lightThemeBackground = Color(0xFFF3F5F9);
+  static const Color darkThemeBackground = Color(0xFF1A1F23);
+
+  //FollowUpColors
+  static const Color FULightYellow = Color(0xFFFBAF00);
+  static const Color FUDarkYellow = Color(0xFFFFAA33);
+  static const Color FULightGrey = Color(0xFF53565A);
+  static const Color FUDarkGrey = Color(0xFF424242);
+
+  //Assets
+  static ElevatedButtonThemeData lightElevatedButtonTheme = ElevatedButtonThemeData(style: ElevatedButton.styleFrom(
+    primary: FULightYellow,
+  ));
+  static ElevatedButtonThemeData darkElevatedButtonTheme = ElevatedButtonThemeData(style: ElevatedButton.styleFrom(
+    primary: FUDarkYellow,
+  ));
+
+
+  static ThemeData lightTheme() {
+    final ThemeData lightTheme = ThemeData.light();
+    return lightTheme.copyWith(
+      backgroundColor: lightThemeBackground,
+      scaffoldBackgroundColor: FULightYellow,
+      elevatedButtonTheme: lightElevatedButtonTheme,
+    );
+  }
+
+  static ThemeData darkTheme() {
+    final ThemeData darkTheme = ThemeData.dark();
+    return darkTheme.copyWith(
+      backgroundColor: darkThemeBackground,
+      scaffoldBackgroundColor: FUDarkGrey,
+        elevatedButtonTheme: darkElevatedButtonTheme,
+    );
+  }
+}
+
 //////////////////
 /// Light theme ///
 //////////////////
