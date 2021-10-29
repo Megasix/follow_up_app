4a5c0ee86ee31771edc9ec9bd46f2de461b7b5d4
Megasix <75232015+Megasix@users.noreply.github.com>
2021-10-29 12:32:06 -0400

    Update speed_limit_api_call.dart
    made the function return non nullable objects

--- lib/services/speed_limit_api_call.dart
+++ lib/services/speed_limit_api_call.dart
@@ -5,37 +5,40 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import 'package:http/http.dart' as http;
 
-class SpeedLimit {
-  Future<String?> getSpeedLimitAtPlace(Map placeMap) async {
+class SpeedLimitApiServices {
+  double getSpeedLimitAtPlace(Map placeMap) {
     String speedLimitUrl =
         "http://overpass-api.de/api/interpreter?data=[out:json];way(" +
             placeMap["osm_id"].toString() +
             ");out%3b";
-    //print(speedLimitUrl);
+    print(speedLimitUrl);
+    print(Uri.parse(speedLimitUrl));
+    double speedLimitOnPos=0;
     http.get(Uri.parse(speedLimitUrl)).then((speedLimit_request) {
+      print(speedLimit_request.body);
       Map speedLimit = jsonDecode(speedLimit_request.body);
-      //print(speedLimit_request.body);
       //print(speedLimit);
       List roadInfo = speedLimit.values.elementAt(3);
       Map<String, dynamic> roadInfoElements = roadInfo.last;
       Map<String, dynamic> roadInfoTags = roadInfoElements.values.elementAt(3);
-      String speedLimitOnPos = roadInfoTags.values.elementAt(2).toString();
-      // print(speedLimitOnPos);
-      return speedLimitOnPos;
+      speedLimitOnPos = roadInfoTags.values.elementAt(2).toString() as double;
+      print(speedLimitOnPos);
     });
+    return speedLimitOnPos;
   }
 
-  Future<Map?> getPlaceInfosAtPos (LatLng position) async {
+  Map getPlaceInfosAtPos(LatLng position) {
     String placeUrl =
         "https://nominatim.openstreetmap.org/reverse?format=json&lat=" +
             position.latitude.toString() +
             "&lon=" +
             position.longitude.toString() +
             "&zoom=17";
-    await http.post(Uri.parse(placeUrl)).then((place_request){
-      Map placeMap = jsonDecode(place_request.body);
+    Map placeMap = new Map();
+    http.post(Uri.parse(placeUrl)).then((place_request) {
+      placeMap = jsonDecode(place_request.body);
       //print(placeMap);
-      return placeMap;
     });
+    return placeMap;
   }
 }
