cb2a2d4ba44325bc0d510197fa782b1612cc974b
Barry Lawson <75232015+Megasix@users.noreply.github.com>
2021-10-30 00:41:54 -0400

    Update AppDelegate.swift
    API_kEY hidden

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyDGZS96VClnydF0jjbf9M4bevRHHya8r1I")
+    GMSServices.provideAPIKey("API_KEY")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
