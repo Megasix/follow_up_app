c3572004e74f8fa5da0b2e3305ef7d225f35da0b
Megasix <75232015+Megasix@users.noreply.github.com>
2021-10-29 22:26:01 -0400

    Update speed_limit_api_call.dart
    deleted unused imports

--- lib/services/speed_limit_api_call.dart
+++ lib/services/speed_limit_api_call.dart
@@ -1,9 +1,6 @@
 import 'dart:convert';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:follow_up_app/generated/l10n.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
 
 class SpeedLimitApiServices {
